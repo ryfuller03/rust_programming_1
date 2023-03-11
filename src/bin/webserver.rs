d7928a736a44976d39dcb100da78f65c6abe4d94
Ryan Fuller <fullerra@hendrix.edu>
2023-03-10 21:12:06 -0600

    Web Server 2: Streaming Done

--- src/bin/webserver.rs
+++ src/bin/webserver.rs
@@ -1,8 +1,17 @@
-use std::{net::{TcpListener, TcpStream}, thread, io::{Read, Write}, sync::{Arc, Mutex}, path::PathBuf, env, fs::File};
+use std::{net::{TcpListener, TcpStream}, thread, io::{Read, Write, BufReader}, sync::{Arc, Mutex}, path::PathBuf, env, fs::File};
 
 use anyhow::anyhow;
 
 fn main() -> anyhow::Result<()> {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    let mut stream_enabled = false;
+    if args.len() > 1 {
+        println!("Usage: webserver -s [number of bytes] OR webserver");
+    }
+    if args[0].contains("-s") {
+        println!("Streaming enabled");
+        stream_enabled = true
+    }
     let total_req = Arc::new(Mutex::new(0));
     let reqs = Arc::new(Mutex::new(0));
     let listener = TcpListener::bind("localhost:8888")?;
@@ -15,7 +24,7 @@ fn main() -> anyhow::Result<()> {
                 let mut counter = total_req_2.lock().unwrap();
                 *counter += 1;
             }
-            match handle_client(&mut stream) {
+            match handle_client(&mut stream, stream_enabled) {
                 Ok(_) => {
                     let mut counter = reqs_2.lock().unwrap();
                     *counter += 1;
@@ -37,8 +46,7 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn handle_client(stream: &mut TcpStream) -> anyhow::Result<()> {
-    
+fn handle_client(stream: &mut TcpStream, stream_enabled: bool) -> anyhow::Result<()> {
     let mut message = String::new();
     loop {
         let mut buffer = [0u8; 500];
@@ -71,7 +79,12 @@ fn handle_client(stream: &mut TcpStream) -> anyhow::Result<()> {
 
     let file_length = contents.as_bytes().len();
     let header = format!("HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\nContent-Length: {}\n\n", file_length);
-    send(stream, header, file_string.as_str())?;
+    println!("{header}");
+    if !stream_enabled {
+        streamm(stream, header, file_string.as_str())?;
+    } else {
+        send(stream, header, file_string.as_str())?;
+    }
     Ok(())
 }
 
@@ -81,7 +94,9 @@ fn get_file_string(message: &str) -> anyhow::Result<String> {
         if done {
             return Ok(String::from(something[1..].to_owned()));
         }
-        done = something == "GET";
+        if something == "GET" {
+            done = true;
+        }
     }
     Err(anyhow!("get_file_string failed."))
 }
@@ -91,5 +106,19 @@ fn send(stream: &mut TcpStream, header: String, file: &str) -> anyhow::Result<()
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     stream.write(format!("{header}{contents}").as_bytes())?;
+    Ok(())
+}
+
+fn streamm(stream: &mut TcpStream, header: String, file: &str) -> anyhow::Result<()> {
+    let mut file = File::open(PathBuf::from(file))?;
+    let mut reader = BufReader::new(file);
+    loop {
+        let mut buffer = [0u8; 500];
+        let bytes_read = reader.read(&mut buffer)?;
+        stream.write(&mut buffer[..bytes_read])?;
+        if bytes_read < 500 {
+            break;
+        }
+    }
     Ok(())
 }
